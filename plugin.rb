602075f80f6609498766a553edd8386309de61e4
alex <alex@94ebc6a0-4824-0410-9694-dfc5f61d2d6b>
2007-01-14 07:20:01 +0000

    Initial import
    git-svn-id: https://svn.cardboardrocket.com/action_mailer_layouts@42 94ebc6a0-4824-0410-9694-dfc5f61d2d6b

---
+++ plugin.rb
@@ -0,0 +1,24 @@
+module ActionMailer
+  class Base
+
+    # Specify the layout name
+    adv_attr_accessor :layout
+
+    def render_message(method_name, body)
+      layout = @layout.to_s
+      md = /^([^\.]+)\.([^\.]+\.[^\.]+)\.(rhtml|rxml)$/.match(method_name)
+      layout << ".#{md.captures[1]}" if md && md.captures[1]
+      layout << ".rhtml"
+      if File.exists?(File.join(layouts_path, layout))
+        body[:content_for_layout] = render(:file => method_name, :body => body)
+        ActionView::Base.new(layouts_path, body, self).render(:file => layout)
+      else
+        render :file => method_name, :body => body
+      end
+    end
+  
+    def layouts_path
+      File.join(template_root, 'layouts')
+    end    
+  end
+end
